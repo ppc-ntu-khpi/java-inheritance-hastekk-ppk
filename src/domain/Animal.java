ef3df12ad101eb6f47a2f010ac7c78ba709d9a28
hastekk-ppk <114733030+hastekk-ppk@users.noreply.github.com>
2023-02-19 11:25:34 +0200

    Add files via upload

--- src/domain/Animal.java
+++ src/domain/Animal.java
@@ -4,23 +4,47 @@ public class Animal {
 
     protected String name;
 
+    protected boolean is_a_predator;
+    
     protected int weight;
 
     public Animal() {
         name="generic animal";
         weight=25;
     }
-
-    public void eat() {
-        System.out.println("Animal eating...");
+    
+    public void sleep()
+    {
+        System.out.println(name + " sleeping...");
     }
 
-    public void speak() {
-        System.out.println("Animal speaking...");
+    public void eat() 
+    {
+        System.out.println(name + " eating...");
+    }
+        
+
+    public void speak() 
+    {
+        System.out.println(name + " speaking...");
     }
 
-    @Override
-    public String toString() {
+    public void hunting()
+    {
+        if (is_a_predator == true)
+            System.out.println(name + " hunting...");
+        else
+            System.out.println(name + " it is not a predator");
+    
+    }
+    
+    public void play() 
+    {
+        System.out.println(name + "is playing...");
+    }
+    
+    public String toString() 
+    {
         return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight+" kg";
     }
 
